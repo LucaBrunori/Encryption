06a5aaaccbf112e61cf70c90bd8ed16224cb2436
Luca Brunori <152266599+LucaBrunori@users.noreply.github.com>
2023-12-12 10:21:14 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 class Main {
   public static void main(String[] args) {
     Matrice m1 = new Matrice(5,5);
+    //inserire parola chiave
   Vigenere v1 = new Vigenere(0, 12, 0, 12, m1(5,5));
   }
 }
